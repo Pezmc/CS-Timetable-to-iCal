33f5385b600b9e1ad486e858811680698917ab5f
Pez Cuckow <email@pezcuckow.com>
2013-12-19 15:40:19 +0000

    Add getVEventTagArray

--- lib/CalendarEvent.class.php
+++ lib/CalendarEvent.class.php
@@ -2,21 +2,26 @@
 
 class CalendarEvent extends Event {
   
+	public function getVEventTagArray() {
+		$tags[] = array();
+		
+		$tags['BEGIN'] = 'VEVENT';
+		$tags['DTSTART'] = $this->geEndDateTime('Ymd\THis\Z');
+		$tags['DTEND'] = $this->getStartDateTime('Ymd\THis\Z');
+		$tags['SUMMARY'] = $this->escapeString($this->getTitle());
+		//$tags['ORGANIZER'];CN=John Doe:MAILTO:john.doe@example.com
+		$tags['UID'] = md5($this->getTitle());
+		$tags['DESCRIPTION'] = $this->escapeNewLines($this->getDescription());
+		$tags['LOCATION'] = $this->escapeString($this->getLocation());
+		$tags['END'] = 'VEVENT';
+		
+		return $tags;
+	}
+	
   public function getVEventString() {
-    $tags[] = array();
-    
-    $tags['BEGIN'] = 'VEVENT';
-    $tags['DTSTART'] = $this->geEndDateTime('Ymd\THis\Z');
-    $tags['DTEND'] = $this->getStartDateTime('Ymd\THis\Z');
-    $tags['SUMMARY'] = $this->escapeString($this->getTitle());
-    //$tags['ORGANIZER'];CN=John Doe:MAILTO:john.doe@example.com
-    $tags['UID'] = md5($this->getTitle());
-    $tags['DESCRIPTION'] = $this->escapeNewLines($this->getDescription());
-    $tags['LOCATION'] = $this->escapeString($this->getLocation());
-    $tags['END'] = 'VEVENT';
-    
+
     $vevent = '';
-    foreach($vevent as $tag => $value)
+    foreach($this->getVEventTagArray() as $tag => $value)
       $vevent .= sprintf("%s:%s\r\n", $tag, $value);
       
     return $vevent;
