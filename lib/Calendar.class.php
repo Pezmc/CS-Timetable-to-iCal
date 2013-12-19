1688738ddf6ad31e1ce2d545fe9c29da4187b52b
Pez Cuckow <email@pezcuckow.com>
2013-12-19 15:59:02 +0000

    Creation of calendar from objects

--- lib/Calendar.class.php
+++ lib/Calendar.class.php
@@ -8,40 +8,48 @@ class Calendar {
   	$this->events[] = $event;
   }
 
-  public function ServeFile() {
-  // Forces file download instead of web page
-  header('Content-type: text/calendar; charset=utf-8');
-  header('Content-Disposition: inline; filename=calendar.ics');
+  public function downloadVCalendar() {
+  	// Forces file download instead of web page
+  	header('Content-type: text/calendar; charset=utf-8');
+  	header('Content-Disposition: inline; filename=calendar.ics');
 
-  // iCal header
-  print("BEGIN:VCALENDAR\r\n");
-  print("VERSION:2.0\r\n");
-  print("PRODID:-//hacksw/handcal//NONSGML v1.0//EN\r\n");
-  print("UID:" . md5(uniqid(mt_rand(), true)) . "@PezCuckow.com\r\n");
-
-  // Basic log
-  $fh = @fopen("simpleLog.txt", 'a');
-
-  // For every row in the input
-  foreach($rows as $row){
-
-    print("BEGIN:VEVENT\r\n");
-    print("DTSTART:".date("Ymd", $start)."T".date("His", $start)."\r\n");
-    print("DTEND:".date("Ymd", $end)."T".date("His", $end)."\r\n");
-    print("SUMMARY:".smartTruncate($data[1], 40)." (".$data[0].")\r\n");
-    print("DESCRIPTION:Title: ".$data[1]
-                        ."\\nCode: ".$data[0]
-                        ."\\nSeat: ".$data[4]
-                        ."\\nStart: ".$data[5]
-                        ."\\nEnd: ".$data[6]
-                        ."\\n\\nRaw\\n". str_replace("\t", " - ", $row)."\r\n");
-    print("LOCATION:".$data[3]."\r\n");
-    print("END:VEVENT\r\n");
-
+		echo $this->createVCalendar();
   }
-  @fclose($fh);
-
-  print("END:VCALENDAR\r\n");
+  
+  public function createVCalendar() {
+  	$vcalendar = $this->tagsToVCalendar($this->getHeaderTags());
+  	
+  	/* @var $event CalendarEvent */
+  	foreach($this->events as $event) {
+  		$vcalendar .= $this->tagsToVCalendar($event->getVEventTagArray());
+  	}
+  	
+  	$vcalendar .= $this->tagsToVCalendar($this->getFooterTags());
+  	
+  	return $vcalendar;
+  }
+  
+  private function getHeaderTags() {
+  	// iCal header
+  	$tags = array();
+  	$tags['BEGIN'] = 'VCALENDAR';
+  	$tags['VERSION'] = '2.0';
+  	$tags['PRODID'] = "-//Pez Cuckow//CS Timetable to Ical//EN";
+  	$tags['UID'] = md5(uniqid(mt_rand(), true)) . "@PezCuckow.com";
+  	return $tags;
+  }
+  
+  private function getFooterTags() {
+  	$tags["END"] = 'VCALENDAR';
+  	return $tags;
+  }
+  
+  private function tagsToVCalendar($tags) {
+  	$vcalendar = '';
+  	foreach($tags as $tag => $value)
+  		$vcalendar .= strtoupper($tag) . ':' . $value . "\r\n";
+  	
+  	return $vcalendar;
   }
 }
 
