0d884c7941c55e0f2d2bca1e11abbf1e2d49ffc9
Pez Cuckow <email@pezcuckow.com>
2013-12-24 11:31:31 +0000

    Move twig cache location

--- ajax.php
+++ ajax.php
@@ -18,7 +18,7 @@ require_once 'Twig/lib/Twig/Autoloader.php';
 Twig_Autoloader::register();
 $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader, array(
-		'cache' => 'cache',
+		'cache' => 'cache/Twig',
 		'debug' => false
 ));
 
